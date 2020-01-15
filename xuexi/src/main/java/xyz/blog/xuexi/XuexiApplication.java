f30c7dfe85993e3ce2b161c8feaaaf3f490b84a9
zx1 <zx1092771091zx@163.com>
2020-01-15 14:01:01 +0800

    添加

---
+++ xuexi/src/main/java/xyz/blog/xuexi/XuexiApplication.java
@@ -0,0 +1,13 @@
+package xyz.blog.xuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuexiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuexiApplication.class, args);
+    }
+
+}
---
+++ xuexi/src/test/java/xyz/blog/xuexi/XuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.blog.xuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
